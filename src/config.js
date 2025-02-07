81957396fa3d38f05d85d2d6a694a934e1edf3f9
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-06 22:39:08 -0300

    :sparkles: feat:Add environment variable validation to configuration

--- src/config.js
+++ src/config.js
@@ -1,7 +1,15 @@
-require('dotenv').config(); 
+require("dotenv").config();
+
+const requiredEnvVars = ["PORT", "TARGET"];
+
+requiredEnvVars.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`${envVar} não foi definida no arquivo .env`);
+  }
+});
 
 const getEnv = (key) => process.env[key];
 
 module.exports = {
-    getEnv, 
-}
+  getEnv,
+};
